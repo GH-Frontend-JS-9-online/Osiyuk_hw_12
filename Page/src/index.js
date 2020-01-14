f8c94a2ffb257cb02df44e9846f184c6b208eb8c
Maxim Osiyuk <dalannok@gmail.com>
2020-01-15 00:29:55 +0200

    Homework 12 not ready

---
+++ Page/src/index.js
@@ -0,0 +1,8 @@
+
+import './js/main.js'
+import './js/sigin.js'
+import './css/main.css'
+import './scss/main.scss'
+
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
